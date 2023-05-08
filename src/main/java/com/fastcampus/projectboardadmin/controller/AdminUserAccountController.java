6353a6e8c5a634a53ecee4c7d4d20cf2d3e7f685
imsadboy <wos961129@gmail.com>
2023-05-08 22:02:29 +0900

    #8 - 뷰 컨트롤러, 기본 뷰, 테스트 기초 작성
    어드민에서 필요로 한 뷰는 다음 5가지
    
    * 메인 루트 뷰
    * 게시글 관리 뷰
    * 댓글 관리 뷰
    * 회원 관리 뷰
    * 어드민 회원 뷰
    
    이에 각 뷰를 담당할 컨트롤러 클래스와
    기본적인 최소한의 핸들러 메소드, 뷰 템플릿,
    정상적인 뷰 응답 케이스를 간단히 정의하는 컨트롤러 테스트 작성
    테스트만 통과하는 정도의 구현이고
    실제 비즈니스 로직 구현을 담고 있지 않음

---
+++ src/main/java/com/fastcampus/projectboardadmin/controller/AdminUserAccountController.java
@@ -0,0 +1,9 @@
+package com.fastcampus.projectboardadmin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin/member")
+@Controller
+public class AdminUserAccountController {
+}
